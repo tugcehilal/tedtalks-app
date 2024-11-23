fdd8d3e843ea51e935c56fd49ffa571e206e5782
tugce_hilal <tugce.hilal.cil@gmail.com>
2024-11-23 15:41:02 +0300

    Initial commit

---
+++ src/main/java/com/tugce/tedtalksapp/TedtalksAppApplication.java
@@ -0,0 +1,13 @@
+package com.tugce.tedtalksapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TedtalksAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TedtalksAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tugce/tedtalksapp/TedtalksAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tugce.tedtalksapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TedtalksAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
